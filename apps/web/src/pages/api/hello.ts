92199f383da94432cf54fd4eccaa34bb4620ebce
Amr Gad <merodiro@gmail.com>
2022-01-23 01:25:45 +0200

    add prettierrc

--- apps/web/src/pages/api/hello.ts
+++ apps/web/src/pages/api/hello.ts
@@ -1,15 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import prisma from "../../utils/prisma";
+import prisma from '@/utils/prisma'
 
 type Data = {
-  name: string;
-};
+  name: string
+}
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'John Doe' })
 }
