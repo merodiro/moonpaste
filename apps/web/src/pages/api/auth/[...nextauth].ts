284b7d928f10a8281a8d5bb01cb926fe42888195
Amr Gad <merodiro@gmail.com>
2022-01-24 21:02:40 +0200

    feat: add simplelogin provider

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -3,12 +3,14 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '@/utils/prisma'
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: {
+    ...PrismaAdapter(prisma),
+    // @ts-expect-error simplelogin adds `user` to the data
+    linkAccount: ({ user, ...data }) => prisma.account.create({ data }),
+  },
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -29,5 +31,25 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    {
+      id: 'simplelogin',
+      name: 'SimpleLogin',
+      type: 'oauth',
+      wellKnown: 'https://app.simplelogin.io/.well-known/openid-configuration',
+      authorization: { params: { scope: 'openid email' } },
+      idToken: true,
+      checks: ['pkce', 'state'],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+        }
+      },
+      options: {
+        clientId: process.env.SIMPLELOGIN_CLIENT_ID as string,
+        clientSecret: process.env.SIMPLELOGIN_CLIENT_SECRET as string,
+      },
+    },
   ],
 })
