7d3ceccbf62b5ec75452deb0243c387ee025ce86
Raggi <35541698+ragrag@users.noreply.github.com>
2022-01-29 21:17:21 +0100

    remove google & creds

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -13,22 +13,6 @@ export default NextAuth({
     linkAccount: ({ user, ...data }) => prisma.account.create({ data }),
   },
   providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text', placeholder: 'email' },
-        password: { label: 'Password', type: 'password', placeholder: 'password' },
-      },
-      async authorize(credentials, req) {
-        // @todo: auth logic
-
-        return null
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
